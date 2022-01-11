a60c692801a79ec2d3608a5ffc5395358e7e44ec
katielrink <71799430+katielrink@users.noreply.github.com>
2022-01-11 11:47:56 -0600

    update ALLOWED_HOSTS

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['/fathomless-mountain-04143.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['fathomless-mountain-04143.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
